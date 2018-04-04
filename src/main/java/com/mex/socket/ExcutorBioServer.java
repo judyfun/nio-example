c238adc4c10a292032698f2c54383fadb376f0a3
xch <xuchuanhao@mobiexchanger.com>
2018-04-04 19:10:15 +0800

    bio example

--- src/main/java/com/mex/socket/ExcutorBioServer.java
+++ src/main/java/com/mex/socket/ExcutorBioServer.java
@@ -5,38 +5,43 @@ import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * xuchuahao
  * on 2018/4/4.
  * <p>
- * 这种方式的SocketServer每次只能处理一个客户端的线程，如果当前一个线程阻塞，那么后一个客户端就被阻塞
+ * 利用线程池，增强socket的并发性，但是依然有弊端，1，耗性能，2.当客户端突然激增到比线程池的数量还多的时候，或者线程池中的某些线程阻塞了，
+ * 也会导致后面新进来的客户端阻塞
  */
-public class BioServer {
+public class ExcutorBioServer {
 
     public static void main(String[] args) throws IOException {
+        ExecutorService pool = Executors.newCachedThreadPool();
         ServerSocket server = new ServerSocket();
-        server.bind(new InetSocketAddress(9898));
-        System.out.println("服务器启动...");
+        server.bind(new InetSocketAddress("127.0.0.1", 9898));
+        System.out.println("服务器启动了。。。");
 
         while (true) {
-            // 获取一个套接字 （阻塞）
             Socket socket = server.accept();
-            System.out.println("来了一个客户端...");
-
-            handle(socket);
+            System.out.println("新接入一个客户端");
+            pool.execute(() -> {
+                handle(socket);
+            });
         }
+
     }
 
     private static void handle(Socket socket) {
+
         try {
-            byte[] bytes = new byte[1024];
             InputStream inputStream = socket.getInputStream();
+            byte[] bytes = new byte[1024];
             while (true) {
-                // 读取数据 （阻塞）
                 int read = inputStream.read(bytes);
                 if (read != -1) {
-                    System.out.println("接收的信息为：" + new String(bytes, 0, read));
+                    System.out.println("接收到的信息为：" + new String(bytes, 0, read));
                 } else {
                     break;
                 }
@@ -45,8 +50,8 @@ public class BioServer {
             e.printStackTrace();
         } finally {
             try {
-                System.out.println("socket 关闭");
                 socket.close();
+                System.out.println("关闭客户端。。。");
             } catch (IOException e) {
                 e.printStackTrace();
             }
